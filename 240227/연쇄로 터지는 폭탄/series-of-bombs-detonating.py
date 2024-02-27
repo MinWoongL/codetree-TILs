1c6648fff48f58afdcc76ecb09e52e602b69e110
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-02-27 15:14:43 +0900

    Title: 연쇄로 터지는 폭탄, Time: 162ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240227/연쇄로 터지는 폭탄/series-of-bombs-detonating.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+
+
+def bt(left, right, score, R):
+    global ans
+    if not left and not right:
+        if score > ans:
+            ans = score
+        return
+
+    if left:
+        m_r = max(0, left - R)
+        for i in range(m_r, left):
+            if i in bombs:
+                left = i
+                score = score + 1
+                break
+        else:
+            left = False
+    if right:
+        M_r = min(1000000000, right + R)
+        for i in range(right + 1, M_r + 1):
+            if i in bombs:
+                right = i
+                score = score + 1
+                break
+        else:
+            right = False
+
+    bt(left, right, score, R + 1)
+
+
+N = int(input())
+
+bombs = []
+for _ in range(N):
+    bombs.append(int(input()))
+
+ans = 0
+
+for b in bombs:
+    bt(b, b, 0, 1)
+
+print(ans)
